1e5b1335bb83d37c415f6d5494f14f67a34dff40
chandanbhowmick <chandan.bhowmick@justdial.com>
2024-01-14 00:07:07 +0530

    pushing basic user service

---
+++ src/test/java/com/amartechs/user/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amartechs.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
